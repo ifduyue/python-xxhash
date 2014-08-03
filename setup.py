d6445ab465f204ce784a4906c544b1b47a70193c
Yue Du <ifduyue@gmail.com>
2014-08-03 22:41:31 +0800

    Add Python 3 classifiers

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
