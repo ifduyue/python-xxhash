3a57e8cb96aeb0f555fe827271040a9ddb386ed5
Yue Du <ifduyue@gmail.com>
2014-11-11 13:53:37 +0800

    Update identifiers

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
