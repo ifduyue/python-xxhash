d4ed9115fd79d884d0e47aef66970901e7432e53
Yue Du <ifduyue@gmail.com>
2018-10-04 19:03:43 +0800

    2018

--- xxhash/cpython.c
+++ xxhash/cpython.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2017, Yue Du
+ * Copyright (c) 2014-2018, Yue Du
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
