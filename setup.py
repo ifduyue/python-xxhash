753d8e14d1b2e06fb3ab5f750f78adc068d7c535
Yue Du <ifduyue@gmail.com>
2018-10-21 21:35:52 +0800

    Automatically build wheels and deloy them to PyPI

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ ext_modules = [
 
 setup(
     # xxhash wasn't available on test.pypi.org
-    name='xxhash' if not os.getenv('XXHASH_BUILDWHEELS') else 'xxhash-test'
+    name='xxhash' if not os.getenv('XXHASH_BUILDWHEELS') else 'xxhash-test',
     version=VERSION,
     description="Python binding for xxHash",
     long_description=open('README.rst', 'r').read(),
