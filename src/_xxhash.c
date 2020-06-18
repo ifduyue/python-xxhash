b42a46a2872c47f0a984635ea1316b9087cd6694
Yue Du <ifduyue@gmail.com>
2020-06-18 15:54:32 +0800

    2020

--- src/_xxhash.c
+++ src/_xxhash.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2019, Yue Du
+ * Copyright (c) 2014-2020, Yue Du
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
