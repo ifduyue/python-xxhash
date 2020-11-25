5e94de5161ca4d47b3d98d4073750c32a8a45c5d
Xie Yanbo <xieyanbo@gmail.com>
2020-11-25 16:40:22 +0800

    Python 3.9

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
     python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*",
